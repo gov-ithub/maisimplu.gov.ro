bd85721930833f01c9e215fd00684285020675dd
Balasa Sorin Stefan <bsorin.stefan@gmail.com>
2016-11-18 16:42:10 +0200

    Optimized header.php code
    Implements #6

--- site/htdocs/wp-content/themes/mai-simplu/header.php
+++ site/htdocs/wp-content/themes/mai-simplu/header.php
@@ -47,13 +47,9 @@
 </head>
 
 <?php if(isset($_POST['scrollPosition'])): ?>
-
 	<body <?php body_class(); ?> onLoad="window.scrollTo(0,<?php echo intval($_POST['scrollPosition']); ?>)">
-
 <?php else: ?>
-
 	<body <?php body_class(); ?> >
-
 <?php endif;
 
 	global $wp_customize;
@@ -81,161 +77,88 @@
 
 
 <header id="home" class="header">
-
 	<div id="main-nav" class="navbar navbar-inverse bs-docs-nav" role="banner">
-
 		<div class="container">
-
 			<div class="navbar-header responsive-logo">
-
 				<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-
-				<span class="sr-only"><?php _e('Toggle navigation','mai-simplu'); ?></span>
-
-				<span class="icon-bar"></span>
-
-				<span class="icon-bar"></span>
-
-				<span class="icon-bar"></span>
-
+					<span class="sr-only"><?php _e('Toggle navigation','mai-simplu'); ?></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
 				</button>
 
 				<?php
 
 					$zerif_logo = get_theme_mod('zerif_logo');
-
 					if(isset($zerif_logo) && $zerif_logo != ""):
-
 						echo '<a href="'.esc_url( home_url( '/' ) ).'" class="navbar-brand">';
-
 							echo '<img src="'.esc_url( $zerif_logo ).'" alt="'.esc_attr( get_bloginfo('title') ).'">';
-
 						echo '</a>';
-
 					else:
 
 						echo '<a href="'.esc_url( home_url( '/' ) ).'" class="navbar-brand">';
-
 							if( file_exists(get_stylesheet_directory()."/images/logo.png")):
-
 								echo '<img src="'.get_stylesheet_directory_uri().'/images/logo.png" alt="'.esc_attr( get_bloginfo('title') ).'">';
-
 							else:
-
 								echo '<img src="'.get_template_directory_uri().'/images/logo.png" alt="'.esc_attr( get_bloginfo('title') ).'">';
-
 							endif;
-
 						echo '</a>';
-
 					endif;
-
 				?>
 
 			</div>
 
-			<nav class="navbar-collapse bs-navbar-collapse collapse" role="navigation"   id="site-navigation">
+			<nav class="navbar-collapse bs-navbar-collapse collapse" id="site-navigation">
 				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'mai-simplu' ); ?></a>
 				<a class="screen-reader-text skip-link" href="#content">Sari la continut</a>
 				<ul id="menu-meniul-1" class="nav navbar-nav navbar-right responsive-nav main-nav-list">
 
-
-
-
 				<?php if ( current_user_can('contributor') ) : ?>
-<li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-146">
-<strong><a href="/institutie/">Meniu Institutie</a></strong>
-<ul class="sub-menu">
+					<li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-146">
+						<strong><a href="/institutie/">Meniu Institutie</a></strong>
+						<ul class="sub-menu">
+							<li><del datetime="2016-07-13T11:57:42+00:00">1. Inregistrare</del>
+								<br />
+									<a href="/institutie/editare-informatii-cont/">Editare cont</a></li>
+									<li><del datetime="2016-07-13T11:57:42+00:00">2. Autentificare</del>
+								<br />
+									<a href="<?php echo wp_logout_url(); ?>">Ieşire</a>
+							</li>
 
-<li><del datetime="2016-07-13T11:57:42+00:00">1. Inregistrare</del>
-<br />
-<a href="/institutie/editare-informatii-cont/">Editare cont</a></li>
+							<li><a href="/completare-informatii-despre-institutia-care-raporteaza/">3. Completare informatii institutie</a></li>
+							<li><a href="/completare-informatii-servicii/">4. Transmitere raport sintetic</a></li>
+							<li><a href="/institutie/transmitere-raport-detaliat-cu-privire-la-serviciile-publice/">5. Transmitere raport detaliat</a></li>
+							<li><a href="/informatii-privind-pcue/">6. Informatii privind PCUe</a></li>
+						</ul>
+					</li>
+				<?php endif; ?>
+					<li id="menu-item-135" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-135"><a href="/">Acasă</a></li>
+				<?php if ( ! is_user_logged_in() ) : ?>
+					<li id="menu-item-136" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-136"><a href="/inregistrare/">Creează cont !</a></li>
+				<?php endif; ?>
 
-<li><del datetime="2016-07-13T11:57:42+00:00">2. Autentificare</del>
-<br />
-<a href="<?php echo wp_logout_url(); ?>">Ieşire</a>
-</li>
+				<?php if ( is_user_logged_in() ) : ?>
+					<li id="menu-item-142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-142"><a href="/propunere/">Propune o îmbunatațire !</a>
+						<ul class="sub-menu">
+							<li id="menu-item-1441" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-144"><a href="/propunere/companie/">Propunere companie</a></li>
+							<li id="menu-item-143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-143"><a href="/propunere/cetateni/">Propunere cetățeni</a></li>
+						</ul>
+					</li>
+				<?php endif; ?>
 
-<li>
-<a href="/completare-informatii-despre-institutia-care-raporteaza/">3. Completare informatii institutie</a>
-</li>
-
-
-<li>
-<a href="/completare-informatii-servicii/">4. Transmitere raport sintetic</a>
-</li>
-
-<li>
-<a href="/institutie/transmitere-raport-detaliat-cu-privire-la-serviciile-publice/">5. Transmitere raport detaliat</a>
-</li>
-
-<li>
-<a href="/informatii-privind-pcue/">6. Informatii privind PCUe</a>
-</li>
-
-
-</ul>
-</li>
-<?php endif; ?>
-
-
-
-
-
-
-
-
-
-
-
-
-				<li id="menu-item-135" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-135"><a href="/">Acasă</li>
-				<?php
-if ( is_user_logged_in() ) { ;?>
-
-<?php } else { ?>
-
-<li id="menu-item-136" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-136"><a href="/inregistrare/">Creează cont !</a>
-</li>
-
-<?php }
-?>
-
-<?php
-if ( is_user_logged_in() ) { ;?>
-
-
-<li id="menu-item-142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-142"><a href="/propunere/">Propune o îmbunatațire !</a>
-<ul class="sub-menu">
-	<li id="menu-item-144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-144"><a href="/propunere/companie/">Propunere companie</a></li>
-	<li id="menu-item-143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-143"><a href="/propunere/cetateni/">Propunere cetățeni</a></li>
-</ul>
-</li>
-<?php } else { ?>
-<?php }
-?>
-<?php
-if ( is_user_logged_in() ) { ;?>
-   <li id="menu-item-145" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-145"><a href="/dashboard/">Propunerile mele</a></li>
-<?php } else { ?>
-<li id="menu-item-145" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-145"><a href="/autentificare/">Autentificare</a></li>
-<?php }
-?>
-<li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-146"><a href="/lista-propuneri/">Toate Propunerile</a>
-<ul class="sub-menu">
-	<li id="menu-item-144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-144"><a href="/lista-propuneri/cele-mai-populare-propuneri/">Cele mai populare</a></li>
-
-</ul></li>
-
-
-
-
-</ul>
+				<?php if ( is_user_logged_in() ) { ?>
+					<li id="menu-item-145" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-145"><a href="/dashboard/">Propunerile mele</a></li>
+				<?php } else { ?>
+					<li id="menu-item-145" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-145"><a href="/autentificare/">Autentificare</a></li>
+				<?php } ?>
+					<li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-146"><a href="/lista-propuneri/">Toate Propunerile</a>
+						<ul class="sub-menu">
+							<li id="menu-item-1442" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-144"><a href="/lista-propuneri/cele-mai-populare-propuneri/">Cele mai populare</a></li>
+						</ul>
+					</li>
+				</ul>
 				<?php //wp_nav_menu( array('theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav navbar-nav navbar-right responsive-nav main-nav-list', 'fallback_cb'     => 'zerif_wp_page_menu')); ?>
-
 			</nav>
-
 		</div>
-
 	</div>
 	<!-- / END TOP BAR -->
